2948f3a84197dffc65116703b14370802c91e6e4
Zumrut-E <zumrut_ekinci@yahoo.ca>
2024-07-10 20:02:55 +0200

    hash added

--- index.js
+++ index.js
@@ -7,23 +7,13 @@ const { check, validationResult } = require('express-validator');
 const { Movie, User } = require('./models');
 const auth = require('./auth')(app);
 
-const port = process.env.PORT || 8080;
-let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+const port = process.env.PORT || 8081
 mongoose.connect('mongodb://localhost:27017/movies_app');
 
 app.use(express.json());
 app.use(passport.initialize());
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 // Protected routes
 app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
@@ -74,48 +64,38 @@ app.get('/directors/:name', passport.authenticate('jwt', { session: false }), as
   }
 });
 
-app.post('/users',
+app.post('/users', [
+  check('Username', 'Username is required').isLength({ min: 5 }),
+  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+  check('Password', 'Password is required').not().isEmpty(),
+  check('Email', 'Email does not appear to be valid').isEmail()
+], async (req, res) => {
+  const errors = validationResult(req);
 
-  [
-    check('Username', 'Username is required').isLength({min: 5}),
-    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
-    check('Password', 'Password is required').not().isEmpty(),
-    check('Email', 'Email does not appear to be valid').isEmail()
-  ],
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
 
-  async (req, res) => {
-    let errors = validationResult(req);
+  try {
+    const hashedPassword = Users.hashPassword(req.body.Password);
 
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array() });
+    const existingUser = await Users.findOne({ Username: req.body.Username });
+    if (existingUser) {
+      return res.status(400).send(`${req.body.Username} already exists`);
     }
 
-  let hashedPassword = Users.hashPassword(req.body.Password);
-  await Users.findOne({ Username: req.body.Username }) 
-  // Search to see if a user with the requested username already exists
-    .then((user) => {
-      if (user) {
-      //If the user is found, send a response that it already exists
-        return res.status(400).send(req.body.Username + ' already exists');
-      } else {
-        Users
-          .create({
-            Username: req.body.Username,
-            Password: hashedPassword,
-            Email: req.body.Email,
-            Birthday: req.body.Birthday
-          })
-          .then((user) => { res.status(201).json(user) })
-          .catch((error) => {
-            console.error(error);
-            res.status(500).send('Error: ' + error);
-          });
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send('Error: ' + error);
+    const newUser = await Users.create({
+      Username: req.body.Username,
+      Password: hashedPassword,
+      Email: req.body.Email,
+      Birthday: req.body.Birthday
     });
+
+    return res.status(201).json(newUser);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(`Error: ${error}`);
+  }
 });
 
 app.put('/users/:username', passport.authenticate('jwt', { session: false }), async (req, res) => {
